ac4fecdcbdd7a57a57aaa6a0567987087d8e640d
Andrew Welsh <decoderandy@gmail.com>
2021-04-08 12:05:55 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,65 +1,54 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Checkout from "./Checkout";
-import Login from "./Login";
-import Payment from "./Payment";
-import Orders from "./Orders";
+import Checkout from './Checkout';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
 import { auth } from "./firebase";
-import { useStateValue } from "./StateProvider";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-const promise = loadStripe(
-  "pk_test_51IURptCwmlG6BzLdXooNvipP6ryaaIBsCAcE2qAEIlq0IYPGkIYEi2Q4RjcHk0UUQuXfZmTf3ldRRNrPlDBwlu1900xN7EQtvw"
-);
 
 function App() {
-  const [{}, dispatch] = useStateValue();
-
+  const [{ user }, dispatch] = useStateValue();
+  // useEffect <- POWERFUL
+  // piece of code that runs based on a give condition
   useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      console.log("THE USER IS >>> ", authUser);
-
-      if (authUser) {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        // the user is logged in
         dispatch({
-          type: "SET_USER",
-          user: authUser,
+          type: 'SET_USER',
+          user: authUser
         });
       } else {
+        // the user is logged out
         dispatch({
-          type: "SET_USER",
-          user: null,
+          type: 'SET_USER',
+          user: null
         });
       }
     });
+
+    return () => {
+      // any clean up operations go here
+      unsubscribe();
+    };
   }, []);
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Switch>
-          <Route path="/orders">
-            <Header />
-            <Orders />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/checkout">
+          <Route path='/checkout'>
             <Header />
             <Checkout />
           </Route>
-          <Route path="/payment">
-            <Header />
-            <Elements stripe={promise}>
-              <Payment />
-            </Elements>
+          <Route path='/login'>
+            <Login />
           </Route>
-          <Route path="/">
+          {/* This is the default route */}
+          <Route path='/'>
             <Header />
-            <Home />
+            <Home /> 
           </Route>
         </Switch>
       </div>
